57722bc82da43c520091f890c6e7dc287e82dbfa
tangkehu <329937872@qq.com>
2018-05-04 01:50:16 +0800

    知识库界面设计

--- app/view_know/views.py
+++ app/view_know/views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_login import login_required
 from . import know
 
@@ -15,14 +15,31 @@ def resource():
     return render_template('know/resource.html', active_flg=['know', 'resource'])
 
 
-@know.route('/upload')
+@know.route('/upload', methods=['GET', 'POST'])
 @login_required
 def upload():
-    return render_template('know/upload.html', active_flg=['know', 'resource'])
+    if request.method == 'POST':
+        # 处理业务逻辑
+        print(request.files.get('file').filename)
+        return redirect(url_for('know.resource'))
+    if request.method == 'GET':
+        return render_template('know/upload.html', active_flg=['know'])
 
 
 @know.route('/file_check')
 @login_required
 def file_check():
     return render_template('know/file_check.html', active_flg=['know', 'file_check'])
+
+
+@know.route('/search', methods=['GET', 'POST'])
+@login_required
+def search():
+    if request.method == 'POST':
+        args = '?search_type={}&search_words={}'.format(
+            request.form.get('search_type'), request.form.get('search_words')
+        )
+        return redirect(url_for('know.search')+args)
+    print(request.args)
+    return render_template('know/search_result.html', active_flg=['know'])
 
