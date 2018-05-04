ac7c9b29db2d548166fa541ab6a028c0e163995e
tangkehu <tangkehu@cecgw.cn>
2018-05-04 17:47:02 +0800

    集训系统界面设计

--- app/view_train.py
+++ app/view_train.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from flask_login import login_required
 
 train = Blueprint('train', __name__)
@@ -9,3 +9,31 @@ train = Blueprint('train', __name__)
 @login_required
 def no_train():
     return render_template('train/no_train.html', active_flg=['train'])
+
+
+@train.route('/apply', methods=['GET', 'POST'])
+@login_required
+def apply():
+    if request.method == 'POST':
+        print(request.form.get('resume'))
+        return redirect(url_for('train.apply'))
+    if request.method == 'GET':
+        return render_template('train/apply.html', active_flg=['train'])
+
+
+@train.route('/file/<type_id>')
+@login_required
+def file(type_id):
+    return render_template('train/file.html', active_flg=['train', 'file', int(type_id)])
+
+
+@train.route('/team')
+@login_required
+def team():
+    return render_template('train/team.html', active_flg=['train', 'team'])
+
+
+@train.route('/student')
+@login_required
+def student():
+    return render_template('train/student.html', active_flg=['train', 'student'])
