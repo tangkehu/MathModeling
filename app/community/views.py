63e98ad300e2be13eb0f644b4f3b56f240d14dff
tangkehu <329937872@qq.com>
2018-05-13 08:33:00 +0800

    更替图标源

--- app/community/views.py
+++ app/community/views.py
@@ -1,9 +1,25 @@
-from flask import render_template
+from flask import render_template, request, url_for, redirect
 from flask_login import login_required
 from . import community
 
 
-@community.route('/main')
+@community.route('/main/<words>', methods=['GET', 'POST'])
 @login_required
-def main():
-    return render_template('community/main.html', active_flg=['community'])
+def main(words):
+    if request.method == 'POST':
+        new_words = request.form.get('words')
+        if new_words:
+            words = new_words
+        return redirect(url_for('community.main', words=words))
+    if request.method == 'GET':
+        if words == 'null':
+            words = None
+        else:
+            pass
+        return render_template('community/main.html', active_flg=['community'], words=words)
+
+
+@community.route('/question')
+@login_required
+def question():
+    return 'ok'
