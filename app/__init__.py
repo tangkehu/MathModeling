4d9e987ed759d92eb29bdcfda4c380447d57c63d
tangkehu <329937872@qq.com>
2018-06-25 08:01:28 +0800

    界面时间处理

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ import logging
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_moment import Moment
 from config import config
 
 
 db = SQLAlchemy()
+moment = Moment()
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
@@ -26,6 +28,7 @@ def create_app(config_name):
     config[config_name].init_app(app)
     db.init_app(app)
     login_manager.init_app(app)
+    moment.init_app(app)
 
     from .main import main
     app.register_blueprint(main)
