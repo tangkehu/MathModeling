451af7378fb5a092bf140a49f9d7c2104cc78835
tangkehu <329937872@qq.com>
2018-07-01 19:28:23 +0800

    部署函数更新

--- manage.py
+++ manage.py
@@ -1,18 +1,20 @@
 from flask_script import Manager, Shell
-# from flask_migrate import MigrateCommand, Migrate
+from flask_migrate import MigrateCommand, Migrate
 from app import create_app, db
 
 app = create_app('default')
 manager = Manager(app)
-# migrate = Migrate(app, db)    # 用于数据库迁移
+migrate = Migrate(app, db)    # 用于数据库迁移
 
 
 @manager.command
 def deploy():
     """应用程序的初始化配置，主要是数据库基础数据配置"""
+    from flask_migrate import upgrade
     from app.models import School, Permission, Role, User
-    db.drop_all()
-    db.create_all()
+    # db.drop_all()
+    # db.create_all()
+    upgrade()
     School.insert_basic_schools()
     Permission.insert_basic_permission()
     Role.insert_basic_roles()
@@ -60,7 +62,7 @@ def make_shell_context():
 
 
 manager.add_command('shell', Shell(make_context=make_shell_context))
-# manager.add_command('db', MigrateCommand)    # 用于数据库迁移
+manager.add_command('db', MigrateCommand)    # 用于数据库迁移
 
 if __name__ == '__main__':
     manager.run()
